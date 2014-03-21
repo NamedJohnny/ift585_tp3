b6364f98de3155eb55a610f98d4d55bda217bdca
NamedJohnny <john_forcier2005@hotmail.com>
2014-03-21 17:53:43 -0400

    Création serveur
    Création du projet de serveur

---
+++ ift585_tp3_server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ift585_tp3_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
